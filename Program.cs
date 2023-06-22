71e0fabc81b8de34ca941474b674a692b0be3c5e
Lucas Matheus Souza E Silva <1455155@sga.pucminas.br>
2023-06-21 21:54:44 -0300

    Ex07

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ListaExtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vogais = "aeiouAEIOU";
+            int contador = 0;
+            Console.WriteLine("Escreva quantas letras quiser: ");
+            string n = Console.ReadLine();
+
+            for(int i = 0; i < n.Length; i++)
+            {
+                if(vogais.Contains(n[i]))
+                {
+                    contador++;
+                }
+            }
+            Console.WriteLine($"Temos {n.Length} letras e {contador} vogais.");
+        }
+    }
+}
